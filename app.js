a66da33081ebef7852e4ba1067956bf3983d6731
Eelis Tiitinen <eelis.tiitinen@starasoft.intra>
2026-08-18 13:20:45 +0300

    kansiorakenne luotu

---
+++ app.js
@@ -0,0 +1,29 @@
+// NODE-PALVELIN
+// =============
+
+
+// KIRJASTOT
+// ---------
+
+const express = require("express");
+const {engine} = require("express-handlebars");
+
+// SOVELLUKSEN ASETUKSET
+// ---------------------
+
+const PORT = process.env.PORT || 8000;
+
+// Luo sovelluksen
+const app = express();
+
+// URL-REITIT
+// ----------
+
+// Kotisivun reitti
+
+// Infosivun reitti
+
+// Yhteistietosivun reitti
+
+// SUORITA SOVELLUS JA TALLENNA MUUTOKSET
+// --------------------------------------
